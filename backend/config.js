ce217b2cc8e111362bfa6105c3d53b34308a1687
LAHIRU UDAYAKUMARA <lahiruudayakumara950@gmail.com>
2023-09-18 19:27:21 +0530

    commit create and update file in backend

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://lahiruudayakumara950:lahiruudayakumara@books-store-mern.qir08rt.mongodb.net/books-collection?retryWrites=true&w=majority';
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,49 @@
-import Express from "express";
-import { PORT } from "./config.js";
+import express, { request } from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModels.js";
 
-const app = Express();
+const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  console.log(req);
+  return res.status(234).send("Hello World!");
+});
+
+app.post("/books", async (req, res) => {
+  try {
+    if(
+      !req.body.title ||
+      !req.body.author ||
+      !req.body.publishYear
+    ) {
+      return res.status(400).send({ message: "Please fill all required fields" });
+    };
+  const newBook = {
+    title: req.body.title,
+    author: req.body.author,
+    publishYear: req.body.publishYear,
+  };
+
+  const book = await Book.create(newBook);
+
+  return res.status(201).send(book);
+} catch (error) {
+  console.log(error);
+  res.status(500).send({ message: error.message });
+}
+});
+
+mongoose
+.connect(mongoDBURL)
+.then(() => {
+  console.log("MongoDB Connected😁");
+  app.listen(PORT, () => {
+    console.log(`🚀Server listening on port: ${PORT}`);
+  });
+})
+.catch((err) => {
+  console.log(err);
 });
